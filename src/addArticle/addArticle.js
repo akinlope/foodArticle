53a2e6e43ffbd41370489ce0b8f2b7be5efe19dd
akinlope <akinrimisitt@gmail.com>
2022-08-30 11:25:10 +0100

    having issues with the addArticle (img part)

--- src/addArticle/addArticle.js
+++ src/addArticle/addArticle.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, addDoc, collection } from "firebase/firestore";
-import { getStorage, ref } from "firebase/storage";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyAZtrim4yk9HgaT1IoDp8S60OcuHWNe1Og",
   authDomain: "food-article.firebaseapp.com",
@@ -10,15 +10,25 @@ const firebaseConfig = {
   appId: "1:139394569028:web:5995898a5d9ec3021d7d11",
 };
 initializeApp(firebaseConfig);
-const db = getFirestore();
-const storage = getStorage();
 
 const burger = document.querySelector("#burger");
 const menu = document.querySelector("#menu");
 const addArticle = document.querySelector("form");
 const image = document.querySelector("#img");
+// console.log(image.value);
 
+const db = getFirestore();
 const colRef = collection(db, "articles");
+
+const storage = getStorage();
+const srorageRef  = ref(storage);
+
+const imageRef = ref(storage, "images");
+
+
+
+
+
 
 burger.addEventListener("click", () => {
   if (menu.classList.contains("hidden")) {
@@ -31,6 +41,8 @@ burger.addEventListener("click", () => {
 addArticle.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  const sparkyRef = ref(storage, `images/${image.value}`);
+
   addDoc(colRef, {
     title: addArticle.title.value,
     author: addArticle.author.value,
@@ -39,20 +51,16 @@ addArticle.addEventListener("submit", (e) => {
     .then(() => {
       addArticle.reset();
       console.log("articles has been added ");
+
+
+      uploadBytes(sparkyRef,File).then((s)=>{
+        console.log("uploaded to blob or file")
+      }).catch((err)=> {err.message})
     })
     .catch((err) => {
       console.log(err.message);
     }); 
+    uploadBytes();
 });
 
-// //get data
-// getDocs(colRef)
-// .then((s)=> {
-//     let article = [];
-//     s.docs.forEach((art)=> {
-//         article.push({...art.data(), id: art.id})
-//         console.log(article );
-//     })
-// }).catch((err)=> {
-//     console.log(err.message);
-// })
+
